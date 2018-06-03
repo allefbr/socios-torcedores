15bfe72f7c3ba7f6b740686e613eaaf29c84068c
Allef Br <allefbr21@gmail.com>
2018-06-02 23:04:08 -0300

    ajuste templates e fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import style from "./scss/main.scss";
 
-(() => {        
-    
+(() => {
     const es6 = `Sou um es6`;
-
     console.log( es6 );
-    console.log('olá sou um app');
-}) ();
+})();
